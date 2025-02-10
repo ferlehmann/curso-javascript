aeed26975fd1bce3456af459d5b748b06525628e
Fernando Lehmann <fer.lehmann@outlook.com>
2025-02-10 19:07:29 -0300

    modificado

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 let opcion;
 const listaDeTalles = [];
 const tablaDeTalles = [
-    { medida: 22, talle: 35 },
-    { medida: 23, talle: 36 },
-    { medida: 24, talle: 37 },
-    { medida: 25, talle: 38 },
-    { medida: 26, talle: 39 },
-    { medida: 27, talle: 40 },
-    { medida: 28, talle: 41 },
     { medida: 29, talle: 42 },
     { medida: 30, talle: 43 },
     { medida: 31, talle: 44 },
@@ -23,18 +16,22 @@ function verTallesGuardados() {
 }
 
 function registrarTalle() {
-    const medidaPie = parseFloat(prompt("Ingrese la medida de su pie en cm"));
-    const talleEncontrado = tablaDeTalles.find(t => t.medida === medidaPie);
-    
-    if (talleEncontrado) {
-        alert("Su talle de calzado es: " + talleEncontrado.talle);
+    let opciones = "Seleccione la medida de su pie:\n1. 29 cm\n2. 30 cm\n3. 31 cm\n4. 32 cm";
+    let seleccion = parseInt(prompt(opciones));
+
+    if (seleccion >= 1 && seleccion <= 4) {
+        let opcion = seleccion - 1;
+        let medidaPie = tablaDeTalles[opcion].medida;
+        let talleEncontrado = tablaDeTalles[opcion].talle;
+
+        alert("Su talle de calzado es: " + talleEncontrado);
         const guardar = confirm("¿Desea guardar este talle?");
         if (guardar) {
-            listaDeTalles.push("Medida: " + medidaPie + " cm - Talle: " + talleEncontrado.talle);
+            listaDeTalles.push("Medida: " + medidaPie + " cm - Talle: " + talleEncontrado);
             alert("Tu talle se guardado exitosamente, utiliza el codigo #ZAPAS15 para obtener tu descuento en la proxima compra");
         }
     } else {
-        alert("No se encontró un talle para esa medida de pie");
+        alert("Opción inválida");
     }
 }
 
@@ -52,7 +49,7 @@ do {
             registrarTalle();
             break;
         default:
-            alert("Opción Invalida");
+            alert("Opción inválida");
             break;
     }
 } while (opcion !== 0);
